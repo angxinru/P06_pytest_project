1cc0bf82a5458baadb62d50b2dde32a2336d9641
angxinru <23043886@myrp.edu.sg>
2024-05-21 15:48:46 +0800

    initial commit

---
+++ P06_pytest_project/tests/test_sample pass.py
@@ -0,0 +1,18 @@
+# the following test all pass
+def test_assert_true():
+    assert True
+
+def test_assert_int_value_equality():
+    x = 4
+    y = 4
+    assert x == y
+
+def test_assert_value_equality():
+    x = 4.0
+    y = 4
+    assert x == y
+
+def test_assert_bool_value_equality():
+    x = False
+    y = 3 == 4
+    assert x == y
